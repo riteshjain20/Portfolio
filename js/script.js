fca4a3d58ff1fa6acae3db52ca91c05c6074ab76
Ritesh Jain <rj4380790@gmail.com>
2026-04-06 22:47:26 +0530

    style: enhance navbar interactivity with active link transitions and padding adjustments

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".nav-link").click(function(){
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+    $(".navbar-collapse").collapse("hide");
+  });
+});
